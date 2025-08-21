196061fd5e143ceb2b7f86e0dd58393eb373bc7f
Ian Maddaus <ian.maddaus@progress.com>
2025-08-21 10:17:52 -0400

    Script corrections
    Signed-off-by: Ian Maddaus <ian.maddaus@progress.com>

--- .expeditor/update_hugo_modules_project_promoted.sh
+++ .expeditor/update_hugo_modules_project_promoted.sh
@@ -2,21 +2,8 @@
 
 set -eoux pipefail
 
-# different chef product repos have their documentation in different subdirectories
-# this variable has to be defined so we can copy content from the proper subdirectory
-# that contains the docs content and properly execute the `hugo mod get` command.
-
-product_key="habitat"
-manifest="https://packages.chef.io/files/${EXPEDITOR_TARGET_CHANNEL}/habitat/latest/manifest.json"
-git_sha="$(curl -s $manifest | jq -r -c ".sha")"
-version="$(curl -s $manifest | jq -r -c ".version")"
-
-
-branch="expeditor/update_docs_${product_key}_${git_sha}"
-git checkout -b "$branch"
-
 ###
-# Manage Habitat generated pages
+# Manage Habitat generated documentation
 ###
 
 # Habitat has two generated files (habitat_cli.md and service_templates.md) that
@@ -24,15 +11,29 @@ git checkout -b "$branch"
 # Those two pages are generated and then pushed up to
 # https://packages.chef.io/files/stable/habitat/latest/generated-documentation.tar.gz
 
-# To add these files to chef-web-docs and doc.chef.io, this script uses curl to pull down the
-# generated-documentation.tar.gz file, and then overwrite the blank pages pulled
-# in by Hugo from github.com/habitat-sh/habitat
+# To add these files to this documentation, this script uses curl to pull down the
+# generated-documentation.tar.gz file, and then extract and overwrite the existing pages in this repo.
 
 # See:
 # - https://github.com/habitat-sh/habitat/pull/7993
 # - https://github.com/chef/chef-web-docs/blob/main/content/habitat/habitat_cli.md
 # - https://github.com/chef/chef-web-docs/blob/main/content/habitat/service_templates.md
 
+# Define variables
+
+EXPEDITOR_TARGET_CHANNEL="stable"
+
+product_key="habitat"
+manifest="https://packages.chef.io/files/${EXPEDITOR_TARGET_CHANNEL}/habitat/latest/manifest.json"
+git_sha="$(curl -s $manifest | jq -r -c ".sha")"
+version="$(curl -s $manifest | jq -r -c ".version")"
+
+# Checkout a new branch
+
+branch="expeditor/update_docs_${product_key}_${git_sha}"
+git checkout -b "$branch"
+
+# Extract generated documentation.
 
 curl --silent --output generated-documentation.tar.gz https://packages.chef.io/files/${EXPEDITOR_TARGET_CHANNEL}/habitat/latest/generated-documentation.tar.gz
 tar xvzf generated-documentation.tar.gz --strip-components 1 -C content/reference
@@ -52,13 +53,13 @@ git add .
 
 # give a friendly message for the commit and make sure it's noted for any future
 # audit of our codebase that no DCO sign-off is needed for this sort of PR since
-#it contains no intellectual property
+# it contains no intellectual property
 
-dco_safe_git_commit "Bump Hugo module $product_key to latest $EXPEDITOR_TARGET_CHANNEL release ($git_sha)."
+# dco_safe_git_commit "Bump $product_key docs content to latest $EXPEDITOR_TARGET_CHANNEL release ($git_sha)."
 
-open_pull_request
+# open_pull_request
 
 # Get back to main and cleanup the leftovers - any changed files left over at
 # the end of this script will get committed to main.
-git checkout -
-git branch -D "$branch"
+# git checkout -
+# git branch -D "$branch"
